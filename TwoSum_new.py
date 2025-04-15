14171534fa452c2399243b34f28411d70e98e610
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-04-15 15:02:30 -0500

    Create TwoSum_new.py

---
+++ TwoSum_new.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        res = 0
+        temp = {}
+ 
+        for i in range(len(nums)):
+            res = target - nums[i]
+            if res in temp:
+                return [i, temp[res]]
+            temp[nums[i]] = i
